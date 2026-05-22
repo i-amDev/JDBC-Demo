077996c59b0ad04598ef7fd471eb6b6113f5c23c
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-05-22 13:00:18 +0530

    - Used "try-with-resources" way to establish a connection with "MySQL" database. - Using this way the code is more concise and clear.

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -8,19 +8,10 @@ public class JDBCDemo {
     private static final String PASSWORD = "Dev@4733";
 
     public static void main(String[] args) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             System.out.println("Connected to the database.");
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-                System.out.println("Connection closed!");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
