f3c278c5b7f6e2e3a56cc4af882d3bf31008f3db
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-05-22 17:06:19 +0530

    - Created one private method to insert data in the MySQL DB.

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCDemo {
     private static final String URL = "jdbc:mysql://localhost:3306/jdbc_demo";
@@ -10,6 +11,17 @@ public class JDBCDemo {
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             System.out.println("Connected to the database.");
+            insertStudent(connection, "Tony", "tony@gmail.com");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertStudent(Connection connection, String name, String email) {
+        String sql = "INSERT INTO student (name, email) VALUES ('" + name + "','" + email + "')";
+        try (Statement statement = connection.createStatement()){
+            int rows = statement.executeUpdate(sql);
+            System.out.println("INSERTED: " + rows);
         } catch (SQLException e) {
             e.printStackTrace();
         }
