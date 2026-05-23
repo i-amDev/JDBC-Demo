88d6dad784dc4022e15675a0797b166ebead36ad
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-05-23 16:02:15 +0530

    - Connected the "TransactionDemo" class to the "MySQL" DB.

--- src/main/java/TransactionDemo.java
+++ src/main/java/TransactionDemo.java
@@ -1,5 +1,18 @@
-public class TransactionDemo {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class TransactionDemo {
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbc_demo";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Dev@4733";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            System.out.println("Connected to the database.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
